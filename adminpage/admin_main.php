a2982dda6b638c461635b404858f8b105302a49d
earvin-KIM <xlfksh00@frwks.com>
2021-12-21 11:11:21 +0900

    관리자페이지 부트스트랩-sidebar

--- adminpage/admin_main.php
+++ adminpage/admin_main.php
@@ -37,47 +37,75 @@
 </head>
 
 
-<body class="hold-transition sidebar-mini layout-fixed">
+<body class="sidebar-mini sidebar-collapse">
     <div class="wrapper">
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
+
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="index3.html" class="nav-link">Home</a>
+                    <a href="#" class="nav-link">Home</a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
                     <a href="#" class="nav-link">Contact</a>
                 </li>
-                <li class="nav-item d-none d-sm-inline-block"><a href="admin_logout.php" class="nav-link">로그아웃</a></li>
+
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a href="admin_logout.php" class="nav-link">로그아웃</a>
+                </li>
             </ul>
         </nav>
         <!-- sidebar -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <!-- Brand Logo -->
-            <img src="..\home\img\header_logo-top.png" width=80% alt="cnr logo"
+        <!-- <aside class="main-sidebar sidebar-dark-primary elevation-4"> -->
+        <!-- Brand Logo -->
+        <!-- <img src="..\home\img\header_logo-top.png" width=80% alt="cnr logo"
                 class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">qptrs</span>
+            <span class="brand-text font-weight-light">qptrs</span> -->
 
 
-            <!-- Sidebar -->
-            <div class="sidebar">
-                <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <!-- Sidebar -->
+        <!-- <div class="sidebar"> -->
+        <!-- Sidebar user panel (optional) -->
+        <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
                         <img src="..\home\img\main1_deliv_over.png" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">관리자</a>
                     </div>
+                </div> -->
+
+        <!-- /.sidebar-menu -->
+        <!-- </div> -->
+        <!-- /.sidebar -->
+        <!-- </aside> -->
+        <?php if ($jb_login == true) { ?>
+        <aside class="main-sidebar sidebar-dark-primary elevation-4" style="margin-right:10px;">
+            <!-- Brand Logo -->
+            <a href="../../index3.html" class="brand-link">
+                <img src="..\home\img\header_logo-top.png" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8; width:-webkit-fill-available;">
+                <span class="brand-text font-weight-light"></span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="..\home\img\portfolio\img2.png" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">earvin_kim</a>
+                    </div>
                 </div>
+
+
 
                 <!-- /.sidebar-menu -->
             </div>
             <!-- /.sidebar -->
         </aside>
-        <?php if ($jb_login == true) { ?>
         <section class="content">
             <div class="container-fluid">
                 <div class="small-box bg-info">
@@ -99,10 +127,10 @@
                     </div>
                 </div>
 
-                <div class="small-box bg-danger">
+                <div class="small-box bg-danger" style="height: 100px;">
                     <div class="inner" style="text-align:center;">
                         <a href="admin_board.php">
-                            <h1 style="color:white;">게시물 일괄관리</h1>
+                            <h1 style="color:white; ">게시물 일괄관리</h1>
                         </a>
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
