763bc76591b7b348c1b5964fae6963ba07e24a91
earvin-KIM <xlfksh00@frwks.com>
2022-04-08 09:16:39 +0900

    For reset

---
+++ mes_spring/src/main/java/com/example/mes_spring/MesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.mes_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesSpringApplication.class, args);
+    }
+
+}
